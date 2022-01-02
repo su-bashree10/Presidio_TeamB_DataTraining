2eefcc563d18babb3aaff547821ffa961e42e53a
Saravana Raja <saravanaraja25@gmail.com>
2022-01-02 17:15:33 +0530

    Update Utility.scala

--- src/com/presidio/project/utils/Utility.scala
+++ src/com/presidio/project/utils/Utility.scala
@@ -3,21 +3,8 @@ package com.presidio.project.utils
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 class Utility {
-//  def getSparkSession: SparkSession = {
-//    System.setProperty("hadoop.home.dir", "D:\\data\\winutils")
-//    val spark = SparkSession.builder
-//      .master("local[*]")
-//      .appName("DataSalesProj")
-//      .getOrCreate()
-//    spark
-//  }
-//  def readCSV(path: String): DataFrame = {
-//    val spark = getSparkSession
-//    val df = spark.read.format("csv").option("header", "true").csv(path)
-//    df
-//  }
     def getSparkSession: SparkSession = {
-       System.setProperty("hadoop.home.dir", "D:\\winutils")
+      System.setProperty("hadoop.home.dir", "D:\\winutils")
       val spark = SparkSession.builder
         .master("local[*]")
         .appName("DataSalesProj")
